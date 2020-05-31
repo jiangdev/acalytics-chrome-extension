23ec21a858d99fccc824319841b4eb690fb6aaa6
Darrel Jiang <darrel.jiang.dev@gmail.com>
2020-05-31 17:47:24 -0500

    added some styling to the download button

--- background.js
+++ background.js
@@ -1,13 +1,4 @@
 chrome.runtime.onInstalled.addListener(function () {
-  chrome.storage.sync.set({ color: "#3aa757" }, function () {
-    console.log("The color is green.")
-  })
-  chrome.tabs.query({ active: true, lastFocusedWindow: true }, (tabs) => {
-    let url = tabs[0].url
-    console.log("url: ", url)
-    // use `url` here inside the callback because it's asynchronous!
-  })
-
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
     chrome.declarativeContent.onPageChanged.addRules([
       {
--- popup.js
+++ popup.js
@@ -196,11 +196,6 @@ function createCSVFromResult(result) {
   })
 }
 
-chrome.storage.sync.get("color", function (data) {
-  changeColor.style.backgroundColor = data.color
-  changeColor.setAttribute("value", data.color)
-})
-
 changeColor.onclick = function (element) {
   let color = element.target.value
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
